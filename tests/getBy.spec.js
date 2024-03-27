37caaf716678b087907977b6839eb280af92bc15
Kunal Tatkar <kunaltatkar@Kunals-MacBook-Pro-6.local>
2024-03-28 09:42:56 +1100

    added file

--- tests/getBy.spec.js
+++ tests/getBy.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
  
-test('Playwright Special locators', async ({ page }) => {
+test('Playwright Special  locators', async ({ page }) => {
   
     await page.goto("https://rahulshettyacademy.com/angularpractice/");
     await page.getByLabel("Check me out if you Love IceCreams!").click();
